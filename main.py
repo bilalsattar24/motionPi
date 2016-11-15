99c541690f9cd639fc61c41f8423d27ab6a54d15
bilalsattar24 <bilalsattar24@gmail.com>
2016-11-14 23:31:31 -0800

    added some imports and comments

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#updating this file
+from gpiozero import MotionSensor
+from datetime import datetime
+#need to import twilio and write necessary code
+
+motionDetector = MotionSensor(4)
+
+if motionDetector.motion_detected:
+    #do some stuff here
